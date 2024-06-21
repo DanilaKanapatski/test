df82a631ea1ca0416f7fbcdffcb08496540f4293
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-21 11:25:42 +0300

    did global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {GlobalStyles} from "./Globalstyles/Global";
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-
-    <App />
-
-);
+    <>
+        <App/>
+        <GlobalStyles></GlobalStyles>
+    </>
+)
+;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
