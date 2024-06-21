803d2dbc15e65baee5a10c27269443e9e3a9b2ab
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-21 12:35:50 +0300

    did lesson about props

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,51 @@
 import React from 'react';
 import './App.css';
-import styled from "styled-components";
-import  {Animation} from "./Animation/Animation";
+import styled, {css} from "styled-components";
 
 function App() {
     console.log('App rendering')
     return (
-        <div className={"App"}>
-            <Menu>
-                <ul>
-                    <li><a href="">menu item 1</a></li>
-                    <li><a href="">menu item 2</a></li>
-                    <li><a href="">menu item 3</a></li>
-                </ul>
-            </Menu>
             <Box>
-                <StyledBtn as="a" href={'#'}>link</StyledBtn>
-                <StyledBtn as={Link} href={'#'}>linkcomp</StyledBtn>
-                <StyledBtn>Hello</StyledBtn>
-                <SuperBtn>SuperBtn</SuperBtn>
+                {/*<StyledBtn color = 'red' fontSize="20px">Hello</StyledBtn>*/}
+                {/*<StyledBtn color = 'green'>Hello</StyledBtn>*/}
+                {/*<StyledBtn fontSize="30px">Hello</StyledBtn>*/}
+                <StyledBtn primary>Hello</StyledBtn>
+                <StyledBtn outline>Hello</StyledBtn>
             </Box>
-        </div>
     )
 }
 
 export default App;
 
-const StyledBtn = styled.button`
+type StyledBtnPropsType = {
+    color?: string,
+    fontSize?: string,
+    primary?: boolean,
+    outline?: boolean,
+}
+
+const StyledBtn = styled.button<StyledBtnPropsType>`
     border: none;
-    background-color: #5e2b2b;
+    //background-color: ${props => props.color || "#5e2b2b"};
     padding: 10px 20px;
-    color: snow;
-    font-size: 2rem;
+    //color: snow;
+    font-size: ${props => props.fontSize};
     font-weight: bold;
 
     &:hover {
         background-color: #1c5881;
-        animation: ${Animation} 2s ease-in-out;
     }
     
-    &:last-child {
-        background-color: blue;
-    }
-`
-
-const Link = styled.a`
-    color: snow;
-    font-size: 2rem;
-    font-weight: bold;
-`
-
-const SuperBtn = styled(StyledBtn)`
-    border-radius: 5px;
-    background-color: #80b647;
-    color: #5e2b2b;
+    ${props => props.outline && css<StyledBtnPropsType> `
+        border: 2px solid ${props => props.color || "#5e2b2b"}
+        color: ${props => props.color || "#5e2b2b"}
+        background-color: transparent;
+    `}
     
-    animation: ${Animation} 2s ease-in-out;
+    ${props => props.primary && css<StyledBtnPropsType>`
+        background-color: ${props => props.color || "#5e2b2b"};
+        color: snow;
+    `}
 `
 
 const Box = styled.div`
@@ -68,28 +58,8 @@ const Box = styled.div`
     button {
         cursor: pointer
     }
-
-    ${Link} {
-        cursor: zoom-in;
-    }
     
     @media screen and (max-width: 800px){
         flex-direction: column;
-    }
-`
-
-const Menu = styled.nav`
-    ul {
-        list-style: none;
-        padding: 0;
-        display: flex;
-        
-        li>a {
-            color: green;
-        }
-        
-        li + li {
-            margin-left: 20px;
-        }
     }
 `
